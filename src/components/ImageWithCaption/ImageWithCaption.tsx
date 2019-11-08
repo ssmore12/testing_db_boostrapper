c59079d045e89117c214f8e1911afb924f81850a
Ishaan Gill <ishaan.gill01@gmail.com>
2019-11-07 19:54:49 -0800

    Simple image with a figure caption

---
+++ src/components/ImageWithCaption/ImageWithCaption.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+//felt like this was too simple, but we weren't sure really how we would implement states or props into this one
+
+class ImageWithCaption extends React.Component {
+    render() {
+      return (
+        <figure>
+            <img>src="https://i.pinimg.com/originals/97/f4/56/97f4562ab00502f380a77903f6666113.jpg"
+            width="600" height="540" alt="Bald Eagle"</img>
+            <figcaption>Bald Eagle</figcaption>
+        </figure>
+      );
+    }
+  }
